55852e974743c0427ae526acac1da55841d15c6b
Michael Yudin <mihelitto@gmail.com>
2021-10-17 17:22:40 +0300

    Add payments matching

--- main.py
+++ main.py
@@ -59,6 +59,57 @@ def aggregate_actions(account: Collection) -> list:
     return result
 
 
+def match_payments(accrual: Collection, payment: Collection) -> tuple:
+    lookup_pipeline = [
+        {
+            '$match': {
+                '$expr': {
+                    '$and': [
+                        {'$lte': ['$date', '$$pay_date']}
+                    ]
+                }
+            }
+        },
+        {'$sort': {'date': 1}},
+    ]
+
+    aggregation_pipeline = [
+        {
+            '$lookup': {
+                'from': 'Accrual',
+                'let': {'pay_month': '$month', 'pay_date': '$date'},
+                'pipeline': lookup_pipeline,
+                'as': 'accruals'
+            }
+        },
+        {'$sort': {'date': 1}},
+    ]
+
+    all_accruals = list(accrual.find())
+    payments = payment.aggregate(pipeline=aggregation_pipeline)
+
+    free_payments = []
+    matched_payments = []
+
+    for payment in payments:
+        pay_accruals = payment.pop('accruals', None)
+        if pay_accruals:
+            last_accrual = pay_accruals[-1]
+            if last_accrual['month'] == payment['month']:
+                matched_payments.append([payment, last_accrual])
+                all_accruals.remove(last_accrual)
+                continue
+            else:
+                for accrual in pay_accruals:
+                    if accrual in all_accruals:
+                        matched_payments.append([payment, accrual])
+                        all_accruals.remove(accrual)
+                        break
+        free_payments.append(payment)
+
+    return matched_payments, free_payments
+
+
 def main():
     env = Env()
     env.read_env()
@@ -77,8 +128,13 @@ def main():
     db = client['ClientsDB']
 
     accounts = db['Account']
+    accruals = db['Accrual']
+    payments = db['Payment']
 
     pprint.pprint(aggregate_actions(accounts))
+    matched_payments, unmatched_payments = match_payments(accruals, payments)
+    pprint.pprint(matched_payments)
+    pprint.pprint(unmatched_payments)
 
 
 if __name__ == '__main__':
